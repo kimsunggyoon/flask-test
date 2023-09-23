c55d9b5dee9f42c921cadb5c68ffeb471dd06eaa
sunggyoon <tjdrbs1011@gmail.com>
2023-09-23 14:15:34 +0900

    serving create

---
+++ flask_serving.py
@@ -0,0 +1,26 @@
+import pickle
+import numpy as np
+from flask import Flask,jsonify,request
+
+model = pickle.load(open('./build/model_pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def make_predict():
+    request_body = request.get_json(force=True)
+
+    x_test[request_body['sepal_length'], request_body['sepal_width'],
+           request_body['petal_lenght'],request_body['petal_width']
+           ]
+    
+    x_test = np.array(x_test)
+    x_test = x_test.reshape(1,-1)
+
+    y_test = model.predict(x_test)
+
+    response_body = jsonify(result=y_test.tolist())
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=55000,debug=True)
+    
